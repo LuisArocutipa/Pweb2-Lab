58d6f83e9ce6dda06cb87ca5a09b61c7eedd0aec
LuisArocutipa <69124601+LuisArocutipa@users.noreply.github.com>
2022-05-20 08:01:26 -0500

    corrección de indentado

--- Lab-03/ejercicio01.js
+++ Lab-03/ejercicio01.js
@@ -4,8 +4,8 @@ const app = express();
 
 app.use(express.static('pub'));
 app.listen(3000, () => {
-console.log("Escuchando en: http://localhost:3000")
+    console.log("Escuchando en: http://localhost:3000")
 });
 app.get('/', (request, response) => {
-response.sendFile(path.resolve(__dirname, 'index.html'));
+    response.sendFile(path.resolve(__dirname, 'index.html'));
 });
