1939d8e6ffbe1d0c307f3ace4e41d32972cbfbfe
LuisArocutipa <69124601+LuisArocutipa@users.noreply.github.com>
2022-05-20 10:09:22 -0500

    Ejercicio 03

---
+++ Lab-03/pub2/recitar.js
@@ -0,0 +1,30 @@
+function recitar(markupText) {
+    const url = 'http://localhost:3000/'
+    const data = {
+        text: markupText
+    }
+    console.log(data)
+    const request = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    }
+    http = fetch(url, request)
+    http.then(
+        response => response.json()
+    ).then(
+        data => {
+            document.querySelector("#htmlCode").innerHTML =
+            data.text
+        }
+    )
+}
+document.addEventListener('DOMContentLoaded', function () {
+    const text = document.querySelector('#markupText')
+    document.querySelector('#markupForm').onsubmit = () => {
+        recitar(text.value)
+        return false;
+    }
+})
